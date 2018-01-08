6795d3061edb2aafc7d6e0344950ed2520e7629e
Steve Ulmer <sulmer@touche@taylor.edu>
2018-01-08 15:58:36 -0500

    Fixed bash script for links

--- links.sh
+++ links.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-mdkir ~/public_html
+mkdir ~/public_html
 ln -s ~/src/develop ~/develop
 ln -s ~/src/public_html ~/public_html/develop
 ln ~/src/createcontest.php ~/public_html/createcontest.php
 ln ~/src/createcontest2.php ~/public_html/createcontest2.php
 ln ~/src/index.php ~/public_html/index.php
-ln -s ~/src/dbcreate.sql public_html/dbcreate.sql
+ln -s ~/src/dbcreate.sql ~/public_html/dbcreate.sql
 ln -s ~/src/readme ~/public_html/readme
 ln -s ~/src/lib ~/public_html/lib
