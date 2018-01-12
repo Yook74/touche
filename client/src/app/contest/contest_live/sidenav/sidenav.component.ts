fc1f72c8f48d1077cf9b52fbf85ce4343de7612b
ykdhan <ykdhan@gmail.com>
2018-01-12 11:58:25 -0500

    Manually implemented flex and other styles (color theme), Added menus in sidebar for user

--- client/src/app/contest/contest_live/sidenav/sidenav.component.ts
+++ client/src/app/contest/contest_live/sidenav/sidenav.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'live-sidenav',
-    templateUrl: './sidenav.component.html'
+    templateUrl: './sidenav.component.html',
+    styleUrls: [ '../../contest.component.css' ]
 })
 export class LiveSideNavComponent {
     constructor() { }
