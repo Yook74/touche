ed284b0e489201dcd8e34960f1a2b477f7126c55
Josh Tiegland <jteiglan@touche@taylor.edu>
2018-01-12 13:38:41 -0500

    Added user pages

--- client/src/app/routing.module.ts
+++ client/src/app/routing.module.ts
@@ -5,6 +5,10 @@ import { CreateContestComponent } from './create_contest/create_contest.componen
 import { ContestComponent } from './contest/contest.component';
 import { ContestLiveComponent } from './contest/contest_live/contest_live.component';
 import { LiveSideNavComponent } from './contest/contest_live/sidenav/sidenav.component';
+import { LiveContestDetailComponent } from './contest/contest_live/contest_detail/contest_detail.component';
+import { LiveClarificationsComponent } from './contest/contest_live/clarifications/clarifications.component';
+import { LiveProblemsComponent } from './contest/contest_live/problems/problems.component';
+import { LiveStandingsComponent } from './contest/contest_live/standings/standings.component';
 import { ContestJudgeComponent } from './contest/contest_judge/contest_judge.component';
 import { ContestAdminComponent } from './contest/contest_admin/contest_admin.component';
 
@@ -14,7 +18,15 @@ const appRoutes: Routes = [
         path: 'contest/:contestName', component: ContestComponent, children: [
             { path: 'judge', component: ContestJudgeComponent, data: { route: 'judge' } },
             { path: 'admin', component: ContestAdminComponent, data: { route: 'admin' } },
-            { path: '', component: ContestLiveComponent, data: { route: 'live' } },
+            {
+                path: '', component: ContestLiveComponent, data: { route: 'live' }, children: [
+                    { path: 'contest-detail', component: LiveContestDetailComponent },
+                    { path: 'clarifications', component: LiveClarificationsComponent },
+                    { path: 'problems', component: LiveProblemsComponent },
+                    { path: 'standings', component: LiveStandingsComponent },
+                    { path: '', redirectTo: 'contest-detail', pathMatch: 'full' }
+                ]
+            },
         ]
     },
     { path: '', redirectTo: '/create-contest', pathMatch: 'full' }
