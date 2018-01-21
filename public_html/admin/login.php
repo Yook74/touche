ce55b62a9d947a090410d5e6be2e48bf3aba8452
Josh Teigland <joshteigland@gmail.com>
2018-01-21 18:51:00 -0500

    Added admin and judge login

---
+++ public_html/admin/login.php
@@ -0,0 +1,22 @@
+<?php
+include_once("../lib/header.php");
+include_once("../lib/database.inc");
+include_once("../lib/token.php");
+include("lib/admin_config.inc");
+$link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+$response = '';
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method === 'POST') {
+    $request = file_get_contents('php://input');
+    $request = json_decode($request, true);
+    if ($request['username'] === $admin_user && $request['password'] === $admin_pass) {
+        $token = EncodeToken(array('role' => 'user', 'id' => $teamId));
+        $response = $token;
+    }
+    else {
+        header("HTTP/1.1 401 Unauthorized");
+        exit;
+    }
+}
+echo json_encode($response);
+?>
---
+++ public_html/judge/login.php
@@ -0,0 +1,23 @@
+<?php
+include_once("../lib/header.php");
+include_once("../lib/database.inc");
+include_once("../lib/token.php");
+$link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+$response = '';
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method === 'POST') {
+    $request = file_get_contents('php://input');
+    $request = json_decode($request, true);
+    $query = mysqli_query($link, "SELECT * FROM CONTEST_CONFIG");
+    $row = mysqli_fetch_assoc($query);
+    if ($request['username'] === $row['JUDGE_USER'] && $request['password'] === $row['JUDGE_PASS']) {
+        $token = EncodeToken(array('role' => 'user', 'id' => $teamId));
+        $response = $token;
+    }
+    else {
+        header("HTTP/1.1 401 Unauthorized");
+        exit;
+    }
+}
+echo json_encode($response);
+?>
