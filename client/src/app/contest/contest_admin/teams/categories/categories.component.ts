432fb0e78529ca1e71e7d1c85da1e147764d61dd
Josh Teigland <joshteigland@gmail.com>
2018-01-21 03:29:13 -0500

    Fixed category bug

--- client/src/app/contest/contest_admin/teams/categories/categories.component.ts
+++ client/src/app/contest/contest_admin/teams/categories/categories.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
 
 import { TableColumn } from '../../../../models/table_column';
-import { Category } from '../../../../models/site';
+import { Category } from '../../../../models/category';
 import { CategoryService } from '../../../../services/model_services/category.service';
 import { AdminCategoryEditComponent } from './edit/edit.component';
 import { AdminCategoryDeleteComponent } from './delete/delete.component';
@@ -31,7 +31,7 @@ export class AdminCategoriesComponent {
     addCategory(): void {
         let dialogRef = this.dialog.open(AdminCategoryAddComponent, {
             width: '',
-            data: { }
+            data: {}
         });
 
         dialogRef.afterClosed().subscribe(result => {
