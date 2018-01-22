eed3b0860e603c11e5da86b9b45d1436bec50777
Josh Teigland <joshteigland@gmail.com>
2018-01-21 20:15:35 -0500

    Fixed problem service

--- client/src/app/services/model_services/problem.service.ts
+++ client/src/app/services/model_services/problem.service.ts
@@ -7,19 +7,25 @@ const mockProblems: Problem[] = [
         id: 1,
         name: 'Problem the First',
         location: 'oneproblem',
-        note: 'F#'
+        note: 'F#',
+        hasHTML: false,
+        hasPDF: false
     },
     {
         id: 2,
         name: 'Problem the Second',
         location: 'twoproblem',
-        note: 'A noteworthy note'
+        note: 'A noteworthy note',
+        hasHTML: false,
+        hasPDF: true
     },
     {
         id: 3,
         name: 'Problem the Third',
         location: 'threeproblem',
-        note: 'A notable note'
+        note: 'A notable note',
+        hasHTML: true,
+        hasPDF: true
     }
 ]
 
