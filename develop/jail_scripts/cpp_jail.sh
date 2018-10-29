670917828b0a054bbb9dbcfe2bcabdb9ded09631
Andrew Blomenberg <andrewBlomen@gmail.com>
2018-10-29 16:45:32 -0400

    Removed incorrect copy from cpp script

--- develop/jail_scripts/cpp_jail.sh
+++ develop/jail_scripts/cpp_jail.sh
@@ -15,5 +15,4 @@ cp /lib/x86_64-linux-gnu/libc.so.6 ./cpp_jail/lib/x86_64-linux-gnu/libc.so.6
 cp /lib/x86_64-linux-gnu/libgcc_s.so.1 ./cpp_jail/lib/x86_64-linux-gnu/libgcc_s.so.1
 cp /lib/x86_64-linux-gnu/libm.so.6 ./cpp_jail/lib/x86_64-linux-gnu/libm.so.6
 cp /lib64/ld-linux-x86-64.so.2 ./cpp_jail/lib64/ld-linux-x86-64.so.2
-cp /usr/bin/sh ./cpp_jail/usr/bin/sh
 cp /usr/lib/x86_64-linux-gnu ./cpp_jail/usr/lib/x86_64-linux-gnu
