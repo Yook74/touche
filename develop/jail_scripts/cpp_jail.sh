ebbcc66bffc9241d81d5def576914c3a33481233
Andrew Blomenberg <andrewBlomen@gmail.com>
2018-11-01 12:33:43 -0400

    Rewrote cpp_jail.sh
    The old script was unsuccessfully trying to copy all of usr/bin/x86_64... into the jail.
    This new version only copies the standard c++ library from that directory
    It is also heavily refactored to look more like the new java jail script.

--- develop/jail_scripts/cpp_jail.sh
+++ develop/jail_scripts/cpp_jail.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 if [ ! -d "cpp_jail" ]; then
-	mkdir cpp_jail
+    mkdir cpp_jail
 fi
 
-mkdir ./cpp_jail/usr
-mkdir ./cpp_jail/usr/bin
-mkdir ./cpp_jail/usr/lib
-mkdir ./cpp_jail/bin
-mkdir ./cpp_jail/lib64
-mkdir ./cpp_jail/lib
-mkdir ./cpp_jail/lib/x86_64-linux-gnu
-cp /bin/sh ./cpp_jail/bin/sh
-cp /lib/x86_64-linux-gnu/libc.so.6 ./cpp_jail/lib/x86_64-linux-gnu/libc.so.6
-cp /lib/x86_64-linux-gnu/libgcc_s.so.1 ./cpp_jail/lib/x86_64-linux-gnu/libgcc_s.so.1
-cp /lib/x86_64-linux-gnu/libm.so.6 ./cpp_jail/lib/x86_64-linux-gnu/libm.so.6
-cp /lib64/ld-linux-x86-64.so.2 ./cpp_jail/lib64/ld-linux-x86-64.so.2
-cp /usr/lib/x86_64-linux-gnu ./cpp_jail/usr/lib/x86_64-linux-gnu
+for dir in "usr" "usr/bin" "usr/lib" "bin" "lib64" "lib" "lib/x86_64-linux-gnu" "usr/lib/x86_64-linux-gnu"
+do
+    mkdir ./cpp_jail/$dir
+done
+
+for lib in "libc.so.6" "libm.so.6" "libgcc_s.so.1"
+do
+    cp -lp /lib/x86_64-linux-gnu/$lib ./cpp_jail/lib/x86_64-linux-gnu/
+done
+
+cp -lp /bin/sh ./cpp_jail/bin/sh
+cp -lp /lib64/ld-linux-x86-64.so.2 ./cpp_jail/lib64
+cp -lp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 ./cpp_jail/usr/lib/x86_64-linux-gnu/libstdc++.so.6
