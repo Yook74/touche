bc2403e6cf29f7ccff7f13462e0eaab40060618e
Andrew Blomenberg <andrewBlomen@gmail.com>
2018-11-07 17:27:17 -0500

    Restructure and stub out clarification tests
    I took the existing tests, restructured them, and added new stubs.

---
+++ tests/acceptance/clarifications/ClarificationsCest.php
@@ -0,0 +1,52 @@
+<?php
+
+
+class ClarificationsCest
+{
+    public static $generalClariText = "General clarification words";
+    public static $specificClariText = "Specific clarification words";
+    public static $clariRequestText = "Oh man I am not good with computer plz to halp";
+
+    public function createGeneralClari(JudgeActor $I)
+    {
+        $I->wantTo("Create a general clarification without prompting");
+        $I->login();
+        $I->amOnPage("/judge/clarifications.php");
+        $I->click("Make new Clarification");
+        $I->fillField("response", self::$generalClariText);
+        $I->click("submit");
+        $I->click("Make new Clarification");
+        $I->see(self::$generalClariText);
+    }
+
+    /**
+     * @depends createGeneralClari
+     */
+    public function lookForGeneralClari(TeamActor $I)
+    {
+        $I->wantTo("Look for the general clarification made by the judge");
+        $I->login();
+        $I->lookForClariText(self::$generalClariText);
+    }
+
+    public function requestClari(TeamActor $I)
+    {
+        $I->wantTo("Request a clarification");
+    }
+
+    /**
+     * @depends requestClari
+     */
+    public function respondToClariRequest(JudgeActor $I)
+    {
+        $I->wantTo("Respond to the request for clarification made by a team");
+    }
+
+    /**
+     * @depends respondToClariRequest
+     */
+    public function lookForClariResponse(TeamActor $I)
+    {
+        $I->wantTo("See the response of the judge to my response");
+    }
+}
