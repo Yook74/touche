9b150ad364bfdb20d976706884a5f263626a3e4c
Andrew Blomenberg <andrewBlomen@gmail.com>
2018-11-29 13:11:00 -0500

    Added source files to produce the "Runtime error" response

---
+++ tests/_support/example_submissions/runtime_error/src.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+//adds two numbers
+int main(int argc, char **argv){
+    int* nul = 0;
+    printf("%d\n", *nul);
+}
---
+++ tests/_support/example_submissions/runtime_error/src.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Main{ //it is important that this does not read "public class Main"
+    //adds two numbers
+    public static void main(String[] args){
+        throw new RuntimeException("Hi mom");
+    }
+}
+
