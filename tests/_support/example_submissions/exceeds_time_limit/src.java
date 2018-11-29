7332dd3e4c0ac4b5832a173e4409e6ebec2386e7
Andrew Blomenberg <andrewBlomen@gmail.com>
2018-11-29 12:47:13 -0500

    Added source files to produce the "Exceeds Time Limit" response

---
+++ tests/_support/example_submissions/exceeds_time_limit/src.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main{ //it is important that this does not read "public class Main"
+    //adds two numbers
+    public static void main(String[] args){
+        Scanner reader = new Scanner(System.in);
+        int one, two;
+        one = reader.nextInt();
+		two = reader.nextInt();
+		while(true) {;}
+    }
+}
+
