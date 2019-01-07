4913c776de44277b659502a38e2e926f66cc055c
Andrew Blomenberg <andrewBlomen@gmail.com>
2019-01-07 18:16:52 -0500

    Filled out stubs submission.py
    Submission is now a fully-fledged, if sparse, class.
    I will add more stuff to it when shared functionality shows up.

--- public_html/judge/py_lib/submission.py
+++ public_html/judge/py_lib/submission.py
@@ -1,3 +1,9 @@
+from subprocess import run
+from os import path
+
+from .exceptions import *
+
+
 class Submission:
     lang_max_cpu_time = None
     lang_chroot_dir = None
@@ -5,26 +11,43 @@ class Submission:
     lang_check_bad_words = None
     lang_forbidden_words = None
 
+    def __int__(self, dirs, problem_id, source_name):
+        self.dirs = dirs
+        self.problem_id = problem_id
+        self.source_path = path.join(dirs['queue'], source_name)
+        self.executable_path = None
+        self.base_name, self.source_extension = path.splitext(source_name)
+
     def move_to_judged(self):
-        pass
+        new_source_path = path.join(self.dirs['judged'], self.base_name + self.source_extension)
+        run(['mv', self.source_path, new_source_path])
+        self.source_path = new_source_path
 
     def replace_headers(self):
-        pass
+        raise NotImplementedError()
 
     def check_bad_words(self):
-        pass
+        if not self.lang_check_bad_words:
+            return
+
+        with open(self.source_path, 'r') as handle:
+            file_string = handle.read()
+
+        for word in self.lang_forbidden_words:
+            if word in file_string:
+                raise ForbiddenWordError(word)
 
     def compile(self):
-        pass
+        raise NotImplementedError()
 
     def move_to_jail(self):
         pass
 
     def execute(self):
-        pass
+        raise NotImplementedError()
 
     def move_from_jail(self):
         pass
 
     def judge_output(self):
-        pass
+        raise NotImplementedError()
