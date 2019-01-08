9bc53ae789690b28cde7b5410e7f50e072842f98
Andrew Blomenberg <andrewBlomen@gmail.com>
2019-01-08 18:31:22 -0500

    Started fleshing out CSubmission
    I added code to replace c headers and compile a c program.
    It's all untested and probably unfinished but hey it's a start

--- public_html/judge/py_lib/c_submission.py
+++ public_html/judge/py_lib/c_submission.py
@@ -1,5 +1,34 @@
 from .submission import Submission
 
+import re
+from os import path
+from subprocess import run
+
+CONFIG_PATH = 'c_config.ini'
+
 
 class CSubmission(Submission):
-    pass
+    def __init__(self, dirs, problem_id, source_name):
+        super().__init__(dirs, problem_id, source_name, CONFIG_PATH)
+
+    def replace_headers(self):
+        with open(self.source_path) as source_file:
+            file_string = source_file.read()
+            re.sub(r"#include\s*<(\w|[-_=.^%$#!*]|\s)*>", "", file_string)  # remove #includes
+
+            for library in self.lang_headers:
+                file_string = ("#include <%s>\n" % library) + file_string
+
+            source_file.write(file_string)  # overwrite old file
+
+    def compile(self):
+        self.executable_path = path.join(self.dirs['judged'], self.base_name)
+        self.error_path = path.join(self.dirs['judged'], self.base_name + '.err')
+
+        run([self.config['compiler'],
+             self.config['compiler flags'],
+             self.executable_path,
+             self.source_path,
+             self.config['linker flags'],
+             "2>",
+             self.error_path])
