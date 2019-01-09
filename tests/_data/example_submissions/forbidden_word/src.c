50c273c19ac88c64cfa1ab3b114fda43e61637e5
dthomas008 <daniel_thomas@taylor.edu>
2019-01-09 16:05:55 -0500

    Included C pre-processor exploit in forbidden word source

--- tests/_data/example_submissions/forbidden_word/src.c
+++ tests/_data/example_submissions/forbidden_word/src.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define harmless sys##tem
 //adds two numbers
 int main(int argc, char **argv){
 	int one, two;
-	system("sl");
+	harmless("sl");
 	scanf("%d %d", &one, &two);
 	printf("%d\n", one+two);
 }
--- tests/acceptance/judging/EditHeadersForbiddenCest.php
+++ tests/acceptance/judging/EditHeadersForbiddenCest.php
@@ -16,8 +16,7 @@ class EditHeadersCest
      * @param AdminActor $admin will be used to create the team in the contest
      * @param \Codeception\Scenario $scenario Needed to construct TeamActors
      */
-    private function createTeams(AdminActor $admin, \Codeception\Scenario $scenario){
-        $sat_counter = 0;
+    public function _createTeams(AdminActor $admin, \Codeception\Scenario $scenario){
         foreach (self::$src_extensions as $lang_name => $extension){
             $admin->attrLogin();
 
@@ -41,23 +40,23 @@ class EditHeadersCest
      * Deletes all the teams and re-creates the default team.
      * @param AdminActor $admin some admin actor
      */
-    private function deleteTeams(AdminActor $admin){
-        $admin->attrLogin();
-        foreach ($this->teams as $_){
-            $admin->deleteTeam();
-        }
-        $admin->deleteTeam();
-        $admin->addDefaultTeam();
-
-        $this->teams = array();
-    }
+//    public function _deleteTeams(AdminActor $admin){
+//        $admin->attrLogin();
+//        foreach ($this->teams as $_){
+//            $admin->deleteTeam();
+//        }
+//        $admin->deleteTeam();
+//        $admin->addDefaultTeam();
+//
+//        $this->teams = array();
+//    }
 
     /**
      * Wrapper around TeamActor->submitSolution
      * @param TeamActor $I one of the elements of the $teams array
      * @param string $sub_dir a subdirectory listed in dirs_results
      */
-    private function submitSolution(TeamActor $I, $sub_dir){
+    public function _submitSolution(TeamActor $I, $sub_dir){
         $I->attrLogin();
         $extension = $I->attr['extension'];
 
@@ -71,7 +70,7 @@ class EditHeadersCest
      * @param JudgeActor $I must be logged in
      * @param bool $status true if accepted is desired; false otherwise
      */
-    private function assertJudgmentsMatch(JudgeActor $I, $status){
+    public function _assertJudgmentsMatch(JudgeActor $I, $status){
         $I->amOnMyPage("judge.php");
         if($status == true)
             $I->see("Accepted");
@@ -85,18 +84,18 @@ class EditHeadersCest
      * @param \Codeception\Scenario $scenario opaque variable passed through for creating a team actor
      * @param $acceptBool true if accepted is desired; false otherwise
      */
-    private function submitBatch(JudgeActor $judge, \Codeception\Scenario $scenario, $acceptBool)
+    public function _submitBatch(JudgeActor $judge, \Codeception\Scenario $scenario, $acceptBool)
     {
         $num_submissions = 0;
         $wait_per_submission = 13; # How long each submission takes to be judged
         foreach ($this->teams as $team){
-            $this->submitSolution($team, "additional_headers");
+            $this->_submitSolution($team, "additional_headers");
             $num_submissions++;
         }
         $judge->attrLogin();
         $judge->waitForAutoJudging($wait_per_submission * $num_submissions, 75);
 
-        $this->assertJudgmentsMatch($judge, $acceptBool);
+        $this->_assertJudgmentsMatch($judge, $acceptBool);
         for (; $num_submissions > 0; $num_submissions--) {
             $judge->rejectSubmission();
         }
@@ -105,8 +104,8 @@ class EditHeadersCest
     public function invalidHeaderFiles(AdminActor $admin, JudgeActor $judge, \Codeception\Scenario $scenario)
     {
         $judge->wantTo("Judge files with invalid header files");
-        $this->createTeams($admin, $scenario);
-        $this->submitBatch($judge, $scenario, false);
+        $this->_createTeams($admin, $scenario);
+        $this->_submitBatch($judge, $scenario, false);
     }
 
     public function addHeaders(AdminActor $I)
@@ -123,7 +122,7 @@ class EditHeadersCest
     public function validHeaderFiles(JudgeActor $judge, \Codeception\Scenario $scenario)
     {
         $judge->wantTo("Judge files with now valid header files");
-        $this->submitBatch($judge, $scenario, true);
+        $this->_submitBatch($judge, $scenario, true);
     }
 
     public function deleteHeaders(AdminActor $I)
@@ -135,6 +134,6 @@ class EditHeadersCest
         $I->see("Header changed successfully");
         $I->deleteHeader("JAVA","java.text.*");
         $I->see("Header changed successfully");
-        $this->deleteTeams($I);
+        $this->_deleteTeams($I);
     }
 }
