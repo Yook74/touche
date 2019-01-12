e631d762f2c99f0cbeb11a2a68dfd28e3a97ee2f
Andrew Blomenberg <andrewBlomen@gmail.com>
2019-01-12 12:25:03 -0500

    Made it harder to compile the java samples
    The java sample submissions now contain examples of public classes and multiple classes per file.
    This makes them harder to compile because java insists on having the source file named the same thing as the public class.
    The new cronscript should be upt to the task, however.

--- tests/_data/example_submissions/runtime_error/src.java
+++ tests/_data/example_submissions/runtime_error/src.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main{ //it is important that this does not read "public class Main"
+class Main{
     //adds two numbers
     public static void main(String[] args){
         throw new RuntimeException("Hi mom");
