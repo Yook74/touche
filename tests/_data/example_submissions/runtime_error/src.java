e6a45447b5d0ca15f39589acda64491529e2ceab
Andrew Blomenberg <andrewBlomen@gmail.com>
2019-01-16 15:54:54 -0500

    Made the java runtime error example submission more interesting
    Now it only throws an exception in some cases. This will better test the cronscript

--- tests/_data/example_submissions/runtime_error/src.java
+++ tests/_data/example_submissions/runtime_error/src.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 class Main{
     //adds two numbers
     public static void main(String[] args){
-        throw new RuntimeException("Hi mom");
+		Scanner reader = new Scanner(System.in);
+		int one, two;
+		one = reader.nextInt();
+		if (one == 0)
+			throw new RuntimeException("Hi mom");
+   		two = reader.nextInt();
+   		System.out.println(one+two);
     }
 }
 
