53282daa1140fef71f1889a10cf2e1602a71fb17
dthomas008 <daniel_thomas@taylor.edu>
2019-01-17 12:59:16 -0500

    Changed header files so src.cpp would compile correctly

--- tests/_data/example_submissions/forbidden_word/src.cpp
+++ tests/_data/example_submissions/forbidden_word/src.cpp
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include <fstream>
+#include <stdlib.h>
 using namespace std;
 
 //adds two numbers
 int main(int argc, char **argv){
 	int one, two;
-    fstream fs;
-    fs.open("src.c");
-    fs.close();
+    system("sl");
 	scanf("%d %d", &one, &two);
 	printf("%d\n", one+two);
 }
