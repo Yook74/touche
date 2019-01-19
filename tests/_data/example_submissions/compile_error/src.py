a6342fe9d491f2ae323a6ee83b4ef13a969b0963
yook74 <andrewBlomen@gmail.com>
2019-01-19 10:33:54 -0500

    Added python2 example_submissions and fixed the python3 ones

--- tests/_data/example_submissions/compile_error/src.py
+++
@@ -1,5 +0,0 @@
-line = input()
-one, two = line.split()
-one = int(one))
-two = int(two)
-print(one + two)
--- tests/_data/example_submissions/incorrect_output/src.py
+++ tests/_data/example_submissions/incorrect_output/src.py
@@ -1,4 +1,8 @@
-line = input()
+try:
+    line = input()
+except EOFError:
+    pass
+
 one, two = line.split()
 one = int(one)
 two = int(two)
